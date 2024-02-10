99375569fc0dc09ff7a0986dadcf619d918dee44
SpEXterXD <nirmalyapal404@gmail.com>
2024-02-11 04:24:54 +0530

    Update todo.dart

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -11,12 +11,6 @@ class ToDo {
 
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'Excercise', isDone: true),
-      ToDo(id: '02', todoText: 'Emails', isDone: true),
-      ToDo(id: '03', todoText: 'Work', ),
-      ToDo(id: '04', todoText: 'Meeting', ),
-      ToDo(id: '05', todoText: 'Lunch', ),
-      ToDo(id: '06', todoText: 'Code', ),
     ];
   }
 
